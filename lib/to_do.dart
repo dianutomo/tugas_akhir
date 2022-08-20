45fccbfd5edae3fceee096c46c18be94b0d068cb
Learning Progress <dian.utomo@gmail.com>
2022-08-20 18:15:04 +0700

    modified:   lib/main.dart 	deleted:    lib/main_screen.dart 	new file:   lib/to_do.dart

---
+++ lib/to_do.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class ToDo extends StatefulWidget {
+  const ToDo({Key? key}) : super(key: key);
+
+  @override
+  _ToDoState createState() => _ToDoState();
+}
+
+class _ToDoState extends State<ToDo> {
+  final List<String> _todoList = <String>[];
+  final TextEditingController _textFieldController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('To-Do List'),
+      ),
+      body: ListView(children: _getItems()),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _displayDialog(context),
+        tooltip: 'Add Item',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _addTodoItem(String title) {
+    setState(() {
+      _todoList.add(title);
+    });
+    _textFieldController.clear();
+  }
+
+  //Generate list of item widgets
+  Widget _buildTodoItem(String title) {
+    return ListTile(
+      title: Text(title),
+    );
+  }
+
+  //Generate a single item widget
+  Future<Future> _displayDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Tambahkan Kegiatan anda'),
+            content: TextField(
+              controller: _textFieldController,
+              decoration:
+                  const InputDecoration(hintText: 'Masukan kegiatan disini'),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('TAMBAH'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _addTodoItem(_textFieldController.text);
+                },
+              ),
+              TextButton(
+                child: const Text('BATAL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  List<Widget> _getItems() {
+    final List<Widget> todoWidgets = <Widget>[];
+    for (String title in _todoList) {
+      todoWidgets.add(_buildTodoItem(title));
+    }
+    return todoWidgets;
+  }
+}
